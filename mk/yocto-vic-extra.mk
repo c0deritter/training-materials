6167304b20add02d23653bf5b2b228372db69650
creich <creich@linux.com>
2020-02-02 13:30:19 +0100

    added content for vic-bsp-yocto workshop (including additional stuff put in after the ws)

---
+++ mk/yocto-vic-extra.mk
@@ -0,0 +1,21 @@
+YOCTO_VOICE_EXTRA_SLIDES	= \
+		yocto-basics \
+		yocto-overview \
+		yocto-advanced \
+		yocto-layer \
+		yocto-recipe-basics \
+		yocto-recipe-advanced \
+		yocto-recipe-extra \
+		yocto-licensing \
+		yocto-sdk 
+
+YOCTO_VOICE_EXTRA_LABS    = setup \
+		yocto-first-build \
+		yocto-advanced-configuration \
+		yocto-add-application \
+		yocto-layer \
+		yocto-extend-recipe \
+		yocto-custom-machine \
+		yocto-custom-image \
+		yocto-sdk \
+		yocto-sdk-eclipse
---
+++ mk/yocto-vic.mk
@@ -0,0 +1,25 @@
+YOCTO_VIC_SLIDES    = \
+		first-slides \
+		about-coderitter \
+		vic-preamble \
+		yocto-bsp \
+		yocto-layer-distro \
+		vic-image \
+		vic-tools-and-commands \
+		yocto-resources \
+		vic-qna \
+		vic-practical \
+		last-slides \
+		yocto-advanced \
+		yocto-runtime-package-management
+
+YOCTO_VIC_LABS    = setup \
+		yocto-first-build \
+		yocto-advanced-configuration \
+		yocto-add-application \
+		yocto-layer \
+		yocto-extend-recipe \
+		yocto-custom-machine \
+		yocto-custom-image \
+		yocto-sdk \
+		yocto-sdk-eclipse
